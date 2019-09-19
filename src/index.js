a0dfdeaef4caf6732244ba1bc45d5a31439c8fcb
thesebas <thesebas@thesebas.net>
2019-09-19 21:11:17 +0200

    starting point

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { createFilter, dataToEsm } from 'rollup-pluginutils';
+import * as inilib from 'ini';
+
+export default function ini (options = {}) {
+  const filter = createFilter(options.include, options.exclude);
+
+  return {
+    name: 'ini',
+    transform (ini, id) {
+      if (id.slice(-4) !== '.ini' || !filter(id)) return null;
+
+      return {
+        code: dataToEsm(inilib.parse(ini)),
+        map: {mapping: ''}
+      };
+    }
+  };
+}
